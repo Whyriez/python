8d1a0434e7231ccc53d7d991ce9d56e4f69fcf0c
alimsx <81366596+Whyriez@users.noreply.github.com>
2023-01-31 12:54:06 +0800

    Update 3.bilangan-prima.py

--- 3.bilangan-prima.py
+++ 3.bilangan-prima.py
@@ -5,15 +5,15 @@ print()
 x = int(input('Input satu angka : '))
  
 angka_prima = True
-if((x == 0) or (x == 1)):
+if((x == 0) or (x == 1)): #jika x sama dengan 0 dan x sama dengan 1 maka angkaprima false krna 0 dan 1 dalam matematika bukanlah angka prima
   angka_prima = False
 else:
-  for i in range(2,(x//2)):
-    if ((x % i) == 0):
+  for i in range(2,(x//2)): #perulangan memulai dari 2 hingga x dibagi 2 fungsi ini agar tidak semua bilangan x dibagi dengan 2 hanya sebagian saja
+    if ((x % i) == 0): #cek apakah x habis dibagi dengan i, jika terpenuhi maka angkaprima false
        angka_prima = False
        break
  
-if(angka_prima):
+if(angka_prima): #jika angka prima true
   print(x,'adalah angka prima')
-else:
+else: #jika angka prima selain true
   print(x,'bukan angka prima')
