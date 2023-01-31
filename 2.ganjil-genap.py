035ea2612cbe9c81796d8a0f8a202e94f88ceda5
alimsx <81366596+Whyriez@users.noreply.github.com>
2023-01-31 12:49:37 +0800

    Update 2.ganjil-genap.py

--- 2.ganjil-genap.py
+++ 2.ganjil-genap.py
@@ -4,7 +4,7 @@ print()
  
 x = int(input('Input sebuah angka : '))
  
-if (x % 2 == 0):
+if (x % 2 == 0):  #jika x dibagi dengan 2 itu sama dengan 0 maka bilangan adalah genap jika tidak bilangan dalah ganjil
   print(x,'adalah bilangan genap')
 else:
   print(x,'adalah bilangan ganjil');
